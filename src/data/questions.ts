41666ee24193fdeb4d0bfcd60c3efe1725718637
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-10-10 17:42:58 -0300

    [feat] Atualizando perguntas.

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -2,53 +2,53 @@ import { Question } from "@/types/Question";
 
 export const questions: Question[] = [
     {
-        question: "Como o sistema deve tratar a movimentação de estoque quando o saldo de um produto é zero?",
+        question: "Como o sistema ERP pode ajudar a gerenciar o nível de estoque?",
         options: [
-            'Bloquear a retirada de produtos',
-            'Permitir a retirada de produtos para evitar vendas perdidas',
-            'Alertar o usuário, mas permitir a movimentação',
-            'Cancelar automaticamente a operação'
-        ],
-        answer: 1
-    },
-    {
-        question: "Qual das opções representa uma transação do tipo 'FI' no módulo de Receitas e Despesas?",
-        options: [
-            'Receita (Income)',
-            'Receita Futura (Future Income)',
-            'Despesa (Expense)',
-            'Despesa Futura (Future Expense)'
-        ],
-        answer: 1
-    },
-    {
-        question: "Qual é o principal objetivo do ajuste de estoque no ERP?",
-        options: [
-            'Garantir que as entradas e saídas estejam sempre equilibradas',
-            'Permitir a movimentação de produtos sem acompanhamento de saldo',
-            'Corrigir discrepâncias entre o saldo físico e o saldo registrado no sistema',
-            'Impedir que produtos sem saída sejam retirados do estoque'
+            'Emitindo relatórios semanais de vendas',
+            'Alocando funcionários para a gestão manual do estoque',
+            'Controlando o nível de estoque e alertando sobre a necessidade de reabastecimento',
+            'Bloqueando pedidos de produtos sem estoque'
         ],
         answer: 2
     },
     {
-        question: "O que o módulo de Acompanhamento de Entregas permite que o usuário faça?",
+        question: "O que o sistema ERP permite em relação ao rastreamento de entregas?",
         options: [
-            'Agendar entregas automaticamente',
-            'Verificar o panorama geral do veículo e do motorista em tempo real',
-            'Listar entregas baseadas em filtros como "status"',
-            'Criar um relatório automático de entregas futuras'
+            'Estimar os prazos de entrega com base em médias históricas',
+            'Rastrear o status das entregas em tempo real e otimizar rotas',
+            'Atribuir manualmente entregas para cada motorista',
+            'Definir horários fixos para entregas sem possibilidade de ajustes'
+        ],
+        answer: 1
+    },
+    {
+        question: "Como o ERP pode ajudar no gerenciamento de projetos?",
+        options: [
+            'Enviar lembretes diários para toda a equipe',
+            'Acompanhar apenas o início e o término dos projetos',
+            'Atribuir tarefas e monitorar prazos e progresso de projetos',
+            'Alocar automaticamente recursos humanos'
         ],
         answer: 2
     },
     {
-        question: "Qual funcionalidade o módulo de Tarefas oferece para gestão de prazos críticos?",
+        question: "Como o ERP facilita o controle financeiro de uma empresa?",
         options: [
-            'Notificar automaticamente os responsáveis pelo projeto',
-            'Filtrar as tarefas com base no vencimento e data atual',
-            'Cancelar automaticamente as tarefas atrasadas',
-            'Reatribuir tarefas com base no prazo'
+            'Gerando faturas automaticamente para todos os clientes',
+            'Controlando o fluxo de caixa, contas a pagar e a receber',
+            'Monitorando apenas os custos de produção',
+            'Definindo limites de gastos para os funcionários'
         ],
         answer: 1
+    },
+    {
+        question: "Como o ERP pode integrar o departamento de estoque ao financeiro?",
+        options: [
+            'Atualizando os custos de produtos automaticamente com base nas vendas e compras',
+            'Bloqueando o acesso dos funcionários de um departamento ao outro',
+            'Enviando lembretes de pagamento para fornecedores de produtos em estoque',
+            'Gerando relatórios anuais sobre o inventário e finanças'
+        ],
+        answer: 0
     }
 ];
